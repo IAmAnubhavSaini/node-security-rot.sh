634c5d7e5e5fb2f9977595077639433091376d7c
Anubhav <iamanubhavsaini@gmail.com>
2021-01-21 11:29:04 +0530

    rotations all 26 of them

--- src/rot.sh.js
+++ src/rot.sh.js
@@ -4,16 +4,22 @@ const red = require('@f0c1s/color-red')
 const yellow = require('@f0c1s/color-yellow')
 const blue = require('@f0c1s/color-blue')
 const green = require('@f0c1s/color-green')
-const {rot13, rot47} = require('@f0c1s/security-rot')
+const {rot13, rot47, rot13n, rotations} = require('@f0c1s/security-rot')
 
 const {execSync} = require('child_process')
 
 const input = process.argv[2]
 
 const fns = [rot13, rot47]
+
 console.log(blue('Start'))
 
 fns.map(fn => [input, fn.name, fn(input)])
+    .map(([a, b, c]) => [green(a), yellow(b), red(c)])
+    .forEach(([a, b, c]) => console.log(a, b, c))
+
+rotations(input)
+    .map(({i, rot}) => [input, `rot13n(${i})`, rot])
     .map(([a, b, c]) => [green(a), yellow(b), red(c)])
     .forEach(([a, b, c]) => console.log(a, b, c))
 
